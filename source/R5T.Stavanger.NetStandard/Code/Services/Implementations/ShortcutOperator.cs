9eef91a97d58caf747c606761508dfe0bc4979e4
MinexAutomation <minex.automation@gmail.com>
2020-02-11 21:58:57 -0800

    Added ShortcutOperator implementation of IShortcutOperator.

---
+++ source/R5T.Stavanger.NetStandard/Code/Services/Implementations/ShortcutOperator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+
+namespace R5T.Stavanger.NetStandard
+{
+    public class ShortcutOperator : IShortcutOperator
+    {
+        public string CreateShortcut(string shortcutFilePath, string shortcutTargetPath, string description)
+        {
+            IShellLink link = (IShellLink)new ShellLink();
+
+            // setup shortcut information
+            //link.SetDescription();
+            link.SetPath(shortcutTargetPath);
+
+            // save it
+            IPersistFile file = (IPersistFile)link;
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            file.Save(shortcutFilePath, false);
+
+            return shortcutFilePath;
+        }
+
+        public string GetShortcutDescription(string shortcutFilePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetShortcutTargetPath(string shortcutFilePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetShortcutDescription(string shortcutFilePath, string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetShortcutTargetPath(string shortcutFilePath, string shortcutTargetPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
