e1bb3808be60a898f23d878ad0ada50743f646fc
MinexAutomation <minex.automation@gmail.com>
2020-02-11 21:59:23 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Stavanger.NetStandard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Stavanger.NetStandard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="ShortcutOperator"/> implementation of <see cref="IShortcutOperator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddNetStandardShortcutOperator(this IServiceCollection services)
+        {
+            services.AddSingleton<IShortcutOperator, ShortcutOperator>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="ShortcutOperator"/> implementation of <see cref="IShortcutOperator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IShortcutOperator> AddNetStandardShortcutOperatorAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IShortcutOperator>(() => services.AddNetStandardShortcutOperator());
+            return serviceAction;
+        }
+    }
+}
