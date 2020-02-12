35635826028be682e77470dd17c41e79c720f2cd
MinexAutomation <minex.automation@gmail.com>
2020-02-11 22:26:14 -0800

    Added Startup and Program-as-service.

---
+++ source/R5T.Stavanger.NetStandard.Construction/Code/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using R5T.Chalandri.DropboxRivetTestingData;
+using R5T.Evosmos.CDriveTemp;
+using R5T.Richmond;
+
+using R5T.Stavanger.Default;
+
+
+namespace R5T.Stavanger.NetStandard.Construction
+{
+    public class Startup : StartupBase
+    {
+        public Startup(ILogger<Startup> logger) : base(logger)
+        {
+        }
+
+        protected override void ConfigureServicesBody(IServiceCollection services)
+        {
+            services
+                .AddSingleton<Program>()
+                .AddNetStandardShortcutOperator(
+                    services.AddDefaultShortcutPathConventionsAction())
+                .AddTestingDataDirectoryContentPathsProvider()
+                .AddTemporaryDirectoryFilePathProvider()
+                ;
+        }
+    }
+}
